a8e4bc7dc6596dd48f79fa4ae72b851afa09bb2d
Cajjad <sgmuptm@outlook.com>
2020-12-24 23:40:27 -0500

    cleaning ProducerConsumer logs

--- ProducerConsumerTest.c
+++ ProducerConsumerTest.c
@@ -27,36 +27,36 @@ void producer(void) {
         // producing some alphabets
         data = (i % 20) + 0x41; 
         semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Producer wants to put [%c] in buffer[%d]\n", data, counter);
+        printf(1, "[%d]Producer wants to put '%c' in buffer[%d]\n", i, data, counter);
         semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_aquire(EMPTY_SPACE_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Producer passed empty_space_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Producer passed empty_space_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_aquire(MUTEX_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Producer passed mutex_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Producer passed mutex_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         if (counter < BUFFER_SIZE) {
             buffer[counter] = data;
-            counter = (counter + 1) % BUFFER_SIZE;
             semaphore_aquire(STDOUT_SEMAPHORE);
-            printf(1, "Producer wrote [%c] in buffer[%d]\n", data, counter);
+            printf(1, "Producer WROTE '%c' in buffer[%d]\n", data, counter);
             semaphore_release(STDOUT_SEMAPHORE);
+            counter = (counter + 1) % BUFFER_SIZE;
         }
 
         semaphore_release(MUTEX_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Producer released mutex_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Producer released mutex_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_release(AVAILABLE_DATA_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Producer released available_data_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Producer released available_data_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
     }
 
     semaphore_aquire(STDOUT_SEMAPHORE);
@@ -73,41 +73,41 @@ void consumer(void) {
     
     for (int i = 0; i < REPEAT_NUM; i++) {
         semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Consumer is starting to read new data\n");
+        printf(1, "[%d]Consumer wants to read new data\n", i);
         semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_aquire(AVAILABLE_DATA_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Consumer passed available_data_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Consumer passed available_data_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_aquire(MUTEX_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Consumer passed mutex_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Consumer passed mutex_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         if (counter > 0) {
             data = buffer[counter];
-            counter = counter - 1;
             semaphore_aquire(STDOUT_SEMAPHORE);
-            printf(1, "Consumer read data mutex_semaphore\n");
+            printf(1, "Consumer READ data '%c' from buffer[%d]\n", data, counter);
             semaphore_release(STDOUT_SEMAPHORE);
+            counter = counter - 1;
         }
 
         semaphore_release(MUTEX_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Cosnumer released mutex_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Cosnumer released mutex_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         semaphore_release(EMPTY_SPACE_SEMAPHORE);
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Cosnumer released empty_space_semaphore\n");
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Cosnumer released empty_space_semaphore\n");
+        // semaphore_release(STDOUT_SEMAPHORE);
 
         // consuming the alphabet
-        semaphore_aquire(STDOUT_SEMAPHORE);
-        printf(1, "Cosnumer consumed data [%c]\n", data);
-        semaphore_release(STDOUT_SEMAPHORE);
+        // semaphore_aquire(STDOUT_SEMAPHORE);
+        // printf(1, "Cosnumer consumed data [%c]\n", data);
+        // semaphore_release(STDOUT_SEMAPHORE);
     }
 
     semaphore_aquire(STDOUT_SEMAPHORE);
@@ -123,14 +123,16 @@ int main(int argc, char* argv[]) {
     semaphore_initialize(MUTEX_SEMAPHORE, 1, 0);
     semaphore_initialize(STDOUT_SEMAPHORE, 1, 0);
 
-    printf(1, "All semaphores are intialized\n");
+    printf(1, "\nAll semaphores are intialized\n");
 
     if (fork() == 0) { // producer child
-        producer();
+        // producer();
+        consumer();
     }
     else {
         if (fork() == 0) { // consumer child
-            consumer();
+            // consumer();
+            producer();
         }
         else { // tester parent
             wait();
